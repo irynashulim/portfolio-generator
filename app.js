d0a97b1df74006316a69fe9edb68dc7d395a165e
Iryna <iryna.shulim@gmail.com>
2021-06-28 18:10:36 -0500

    HTML created

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
 const generatePage = (name, github) => {
@@ -18,5 +19,8 @@ const generatePage = (name, github) => {
     </html>
     `;
   };
-  console.log(name, github);
-console.log(generatePage(name, github));
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
