43169d6c2e0d44140aace8e5bc919a03bdd9276e
Iryna <iryna.shulim@gmail.com>
2021-06-30 17:15:07 -0500

    copyFile function update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -127,20 +127,20 @@ Add a New Project
 };
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  return generatePage(portfolioData);
-})
-.then(pageHTML => {
-  return writeFile(pageHTML);
-})
-.then(writeFileResponse => {
-  console.log(writeFileResponse);
-  return copyFile();
-})
-.then(copyFileResponse => {
-  console.log(copyFileResponse);
-})
-.catch(err => {
-  console.log(err);
-});
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
